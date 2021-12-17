649f98aa92b8cc5d7498b5b7f4d0dcb1f33f37e3
wadholm <malin.wadholm@gmail.com>
2021-12-17 19:22:34 +0100

    Updates for Oauth with Google

--- src/pages/auth.js
+++ src/pages/auth.js
@@ -39,35 +39,67 @@ const Auth = (props) => {
   // };
   // console.log(process.env.REACT_APP_GOOGLE_CLIENT_ID);
 
-  const login = async () => {
-    try {
-      const response = await fetch(
-        `${process.env.REACT_APP_BACKEND_URL}/auth/google`,
-        {
-          method: "GET",
-          // mode: "no-cors", // no-cors, *cors, same-origin
+//   (async () => {
+//     const request = await fetch("http://localhost:1337/auth/login/success", {
+//       method: "GET",
+//       credentials: "include",
+//       headers: {
+//        Accept: "application/json",
+//         "Content-Type": "application/json",
+//        "Access-Control-Allow-Credentials": true,
+//     },
+//   });
 
-          headers: {
-            Accept: "application/json",
-            "Content-Type": "application/json",
-            // Authorization: "Bearer " + context.token,
-          },
-          // body: JSON.stringify({}),
-        }
-      );
-      console.log(response);
-      const data = await response.json();
-      if (!response.ok) {
-        throw new Error(data.message);
-      }
-      console.log(data);
-      setSuccess(true);
-      setLoading(false);
-    } catch (err) {
-      setError(err.message);
-      setLoading(false);
-    }
-  };
+//   const res = await request.json();
+//    //In my case I stored user object in redux store
+//    if(request.status === 200){
+//      //Set User in Store
+//       console.log(res);
+//       console.log(request);
+//   }
+
+// })();
+
+  // const login = async () => {
+  //   try {
+  //     const response = await fetch(
+  //       `${process.env.REACT_APP_BACKEND_URL}/auth/google`,
+  //       {
+  //         method: "GET",
+  //         // mode: "no-cors", // no-cors, *cors, same-origin
+
+  //         headers: {
+  //           Accept: "application/json",
+  //           "Content-Type": "application/json",
+  //           // Authorization: "Bearer " + context.token,
+  //         },
+  //         // body: JSON.stringify({}),
+  //       }
+  //     );
+  //     console.log(response);
+  //     const data = await response.json();
+  //     if (!response.ok) {
+  //       throw new Error(data.message);
+  //     }
+  //     console.log(data);
+  //     setSuccess(true);
+  //     setLoading(false);
+  //   } catch (err) {
+  //     setError(err.message);
+  //     setLoading(false);
+  //   }
+  // };
+
+const loginWithGoogle = () => {
+  // ev.preventDefault();
+  window.open("http://localhost:1337/auth/google", "_self");
+}
+
+const logoutWithGoogle = () => {
+  // ev.preventDefault();
+  window.open("http://localhost:1337/auth/logout", "_self");
+}
+
   return (
     <div>
       {/* <GoogleLogin
@@ -78,7 +110,8 @@ const Auth = (props) => {
         cookiePolicy={"single_host_origin"}
         isSignedIn={true}
       /> */}
-      <Button onClick={login}>Logga in</Button>
+      <Button onClick={loginWithGoogle}>Logga in</Button>
+      <Button onClick={logoutWithGoogle}>Logga ut</Button>
     </div>
   );
 };
